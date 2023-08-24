902470848ded75510497a4ff2153aa3be01fb521
Fadhil <79124345+fdhlnrzzmn@users.noreply.github.com>
2023-08-24 22:08:06 +0800

    build: add reactjs using vite and chakra iu

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <ChakraProvider>
+          
+      </ChakraProvider>
+    </>
+  )
+}
+
+export default App
